4c06bd3955050bea0bcc5f78d9c920e480449408
Mikhail Gladinov <gladinovmisha@gmail.com>
2025-12-26 12:27:06 +0300

    feat:add headers and context keys

--- context/keys.go
+++ context/keys.go
@@ -3,3 +3,5 @@ package contextkeys
 type ctxKey struct{}
 
 var TraceIDKey = ctxKey{}
+var EncryptedTokenKey = ctxKey{}
+var ChatIDKey = ctxKey{}
--- http/headers.go
+++ http/headers.go
@@ -1,7 +1,7 @@
 package httpheaders
 
-
 const (
-	HeaderTraceID   = "X-Trace-Id"
-	HeaderChatID    = "X-Chat-Id"
+	HeaderTraceID        = "X-Trace-Id"
+	HeaderChatID         = "X-Chat-Id"
+	HeaderEncryptedToken = "X-Encrypted-Token"
 )
